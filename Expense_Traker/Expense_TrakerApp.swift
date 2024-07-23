0fc867cdf2664e27cd59975d5ede897fe6c153a2
Preet Pambhar <preetpambhar73530@gmail.com>
2024-07-23 01:36:44 -0400

    Initial Commit

---
+++ Expense_Traker/Expense_TrakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrakerApp.swift
+//  Expense_Traker
+//
+//  Created by Preet Pambhar on 2024-07-23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
